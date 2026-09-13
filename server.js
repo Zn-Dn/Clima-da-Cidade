c4719a91404f907878877daebb8d2e69ade6bb2a
Daniel <dani@email.com>
2026-09-13 00:14:28 -0300

    debug: adiciona teste na rota raiz

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const app = express()
 app.use(cors());
 app.use(express.static("public"));
 app.use(express.json())
-
+app.get("/", (req, res) => {
+    res.send("TESTE ROTA RAIZ 999");
+});
 app.get("/", (req, res) => {
     res.sendFile("clima.html", { root: "public" });
 });
