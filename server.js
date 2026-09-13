b5d59bf1947a20266828f2a408c759d6afa5893e
Daniel <dani@email.com>
2026-09-12 22:58:50 -0300

    fix: corrige servidor

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const apiKey = process.env.OPENWEATHER_KEY;
 
 const app = express()
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static("./public"));
 app.use(express.json())
 
 
